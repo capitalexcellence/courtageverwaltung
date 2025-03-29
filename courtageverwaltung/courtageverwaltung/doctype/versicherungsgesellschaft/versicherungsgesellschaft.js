28a27e449912426bbebdb8458ea86a05cfa28b9f
Thies <thies@erpnext.capitalexcellence.de>
2025-03-29 09:06:45 +0100

    courtageabrechnung first steps

---
+++ courtageverwaltung/courtageverwaltung/doctype/versicherungsgesellschaft/versicherungsgesellschaft.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Capital Excellence GmbH and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Versicherungsgesellschaft", {
+// 	refresh(frm) {
+
+// 	},
+// });
